bda191a6fc0096e6cf047d487a88761aa4fad702
vignesh-subramaniam <vignesh.peramaiyadass@freshworks.com>
2019-11-27 22:59:30 +0530

    initial commit

---
+++ app/index.js
@@ -0,0 +1,17 @@
+class App extends React.Component {
+    constructor(props){
+        super(props);
+    }
+    componentDidMount(){
+        
+    }
+    render() {
+        return (
+            <div className="ml8">Hi</div>
+        )
+    }
+}
+ReactDOM.render(
+    <App/>,
+    document.getElementById('spatial-intelligence')
+);
