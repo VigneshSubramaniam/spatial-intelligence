706e4461e3d5e6edf330ce9f563fa67fdb005de1
vignesh-subramaniam <vignesh.peramaiyadass@freshworks.com>
2019-11-29 05:25:28 +0530

    Added build file

---
+++ app/space-intelligence/build/precache-manifest.597bab9139d485e948bf5c9cfd738db7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c0585ad40dc87cf280a61fea650e2a6d",
+    "url": "./index.html"
+  },
+  {
+    "revision": "63fa3ecfd561592d7515",
+    "url": "./static/css/2.b0b3c2c1.chunk.css"
+  },
+  {
+    "revision": "9521e372791b4c6118dc",
+    "url": "./static/css/main.17ff2bc6.chunk.css"
+  },
+  {
+    "revision": "63fa3ecfd561592d7515",
+    "url": "./static/js/2.74c002ff.chunk.js"
+  },
+  {
+    "revision": "9521e372791b4c6118dc",
+    "url": "./static/js/main.dac27067.chunk.js"
+  },
+  {
+    "revision": "7a32fbdde21b956d17e0",
+    "url": "./static/js/runtime-main.276d3f41.js"
+  },
+  {
+    "revision": "c6faa9c196aa2c5d6dad373fb6d9846e",
+    "url": "./static/media/ground_floor.c6faa9c1.png"
+  }
+]);
